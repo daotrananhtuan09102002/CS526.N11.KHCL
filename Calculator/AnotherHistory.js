344f7000220e7956849e9691b2e00360df88bd76
ptatien0307 <anhtien372002@gmail.com>
2022-10-19 16:11:38 +0700

    ptatien commit

---
+++ Calculator/AnotherHistory.js
@@ -0,0 +1,105 @@
+import { StyleSheet, Text, View, ScrollView, TextInput, Pressable, Dimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import React, { useState, useEffect } from 'react';
+export default function CalHistory(props) {
+
+    let myFoundHistory = []
+
+    const [compHistory, setCompHistory] = useState([])
+    const [searchText, setSearchText] = useState('')
+
+
+    useEffect(() => {
+        setCompHistory([...props.myCalHistory])
+    }, [props.myCalHistory])
+
+
+
+    return (
+        <View style={[styles.container,
+        { display: props.myDisplayHistory ? 'none' : 'flex' }]}>
+
+            {/* View search icon and search input text */}
+            <View style={styles.searchContainer}>
+
+                {/* View input text */}
+                <TextInput
+                    style={styles.search}
+                    placeholder="Please enter text..."
+                    onChangeText={inputText => setSearchText(inputText)}>
+                </TextInput>
+            </View>
+
+
+            <View style={styles.scrollContainer}>
+                {
+                    compHistory.filter(element => {
+                        return element.in.includes(searchText) || element.out.includes(searchText)
+                    }).map(element =>
+                        <View style={styles.box}>
+                            <Text style={[styles.inputText, { backgroundColor: element.inFound ? 'rgb(217,129,47)' : 'none', color: element.inFound ? 'black' : 'rgba(128,128,128,0.5)' }]}>{element.in}</Text>
+                            <Text style={[styles.outputText, { backgroundColor: element.outFound ? 'rgb(217,129,47)' : 'none', color: element.outFound ? 'black' : 'white' }]}>{'=' + element.out}</Text>
+                        </View>
+                    )
+                }
+            </View>
+        </View >
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: '100%',
+        height: '67%',
+        backgroundColor: 'rgb(1,1,1)',
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        flexDirection: 'column',
+        position: 'absolute',
+        top: 220
+    },
+
+    scrollContainer: {
+        marginTop: 50,
+        width: '100%',
+        height: Dimensions.get('screen').height,
+        flex: 1,
+    },
+    searchContainer: {
+        borderColor: '#fff',
+        borderWidth: 1,
+        backgroundColor: 'rgb(1,1,1)',
+        width: '100%', // relate to browser
+        height: 50,
+        padding: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        position: 'fixed',
+        top: 220,
+        zIndex: 10,
+    },
+    search: {
+        marginLeft: 16,
+        fontSize: 15,
+        opacity: 0.5,
+        color: '#fff',
+        width: '100%'
+    },
+    inputText: {
+        color: 'rgb(119,119,119)',
+        fontSize: 20,
+    },
+    outputText: {
+        color: 'white',
+        fontSize: 30,
+    },
+    box: {
+        borderBottomColor: 'rgba(128,128,128,0.5)',
+        borderBottomWidth: 2,
+        marginRight: 8,
+        marginBottom: 8,
+        width: '100%',
+        height: 80,
+    }
+})
